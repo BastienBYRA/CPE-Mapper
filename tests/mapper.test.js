6d5e7e3dc2436a45173c5ee96c78f194638fd5a6
Bastien <neitsabast@gmail.com>
2025-09-22 18:13:17 +0200

    refractor(tests): set all exported functions on top of the file instead of each test

--- tests/mapper.test.js
+++ tests/mapper.test.js
@@ -1,12 +1,12 @@
 import test from 'node:test';
 import assert from 'node:assert';
 import { TEST__MAPPER_JS } from '../mapper.js';
+const { getComponentFullName, searchCpeMapping } = TEST__MAPPER_JS
 
 /**
  * Test for `getComponentFullName`
  */
 test('getComponentFullName', async (t) => {
-  const getComponentFullName = TEST__MAPPER_JS.getComponentFullName;
 
   await t.test('returns group:name when both exist', () => {
     assert.strictEqual(getComponentFullName('org.apache', 'lib'), 'org.apache:lib');
@@ -37,8 +37,6 @@ test('searchCpeMapping', async (t) => {
             { name: 'express', cpe: 'cpe:...' }
         ]
     };
-
-    const searchCpeMapping = TEST__MAPPER_JS.searchCpeMapping;
 
     await t.test('searchCpeMapping finds a match with group:name', () => {
         const result = searchCpeMapping('org.apache:lib', cpeDb);
