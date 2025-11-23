b1b33dbf445773ee8a9c689b6b4a7461d4fb3911
Bastien <neitsabast@gmail.com>
2025-11-23 01:23:56 +0100

    chore: bump version to 1.2.0

--- src/cli.js
+++ src/cli.js
@@ -27,7 +27,7 @@ program
   .name('cpe-mapper')
   .description(`CPE-mapper is a CLI tool and JSON-based database designed to accurately map software package names to their corresponding CPEs (Common Platform Enumerations). 
 Its main goal is to improve vulnerability identification in cases where standard package names fail to match known CPEs.`)
-  .version('1.1.0');
+  .version('1.2.0');
 
 program.command('apply')
   .description('Apply CPE mappings to a CycloneDX BOM file')
@@ -35,7 +35,7 @@ program.command('apply')
   .requiredOption('-o, --output-file <file>', 'Output mapped BOM file')
   .option('--no-update', 'Disable updating the CPE Mapping database')
   .option('-v, --verbose', 'Enable verbose logging')
-  .option('--override-cpe', 'Override BOM CPEs with mapped values from our database (Only for CycloneDX BOM file)')
+  .option('--override-cpe', 'Override BOM CPEs with mapped values from our database (CycloneDX only)')
   .action((options) => {
     const { inputFile, outputFile, update, verbose, overrideCpe } = options;
     applyCPEMappings(inputFile, outputFile, update, verbose, overrideCpe, appConfig)
